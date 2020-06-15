f8d759df8e8458c68bdb4161768010ac43113a01
Ali Ben Ali (Laptop) <alijmabe@buffalo.edu>
2020-06-15 17:47:58 -0400

    -Update build scripts.

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
 
-echo "Configuring and building ORB_SLAM2 ..."
+echo "Configuring and building Edge_SLAM ..."
 
 mkdir build
 cd build
--- build_ros.sh
+++ build_ros.sh
@@ -1,6 +1,6 @@
 echo "Building ROS nodes"
 
-cd Examples/ROS/ORB_SLAM2
+cd Examples/ROS/Edge_SLAM
 mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
