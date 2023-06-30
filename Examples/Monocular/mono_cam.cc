941e7ee77d4f8bc458c024d4b1d7eb8d97ab9b19
Zain Nasir <znasir1@binghamton.edu>
2023-06-30 17:09:25 +0500

    added script for getting webcam feed

---
+++ Examples/Monocular/mono_cam.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+#include <opencv2/core/core.hpp>
+#include <System.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    cerr << endl << "Client Usage: ./mono_cam VOC_PATH SETTINGS_PATH client" << endl;
+    cerr << endl << "Server Usage: ./mono_cam VOC_PATH SETTINGS_PATH server" << endl;
+  }
+
+  // Check run type and conver to lowercase
+  std::string RunType(argv[3]);
+  std::transform(RunType.begin(), RunType.end(), RunType.begin(), ::tolower);
+
+  // Run edgeslam for the client
+  if (RunType.compare("client") == 0) {
+    // Open webcam and set 'q' as quit button
+    cv::Mat image;
+    cv::nameWindow("Window", CV_WINDOW_AUTOSIZE);
+    cv::VideoCapture v_cap(0);
+
+    if (!cap.isOpened()) {
+      cerr << endl << "Cannot open camera" << endl;
+    }
+
+    while (true) {
+      v_cap >> image;
+      cv::imshow("Window", image);
+
+      if (cv::waitKey(10) >= 0) {
+	break;
+      }
+    }
+  }
+}
