f66279aa5b7cf4492e514e4cec6c2f9f6c278883
zain <znasir1@binghamton.edu>
2023-08-24 15:41:30 -0400

    isolate video file to only client

--- Examples/Monocular/mono_anafi.cc
+++ Examples/Monocular/mono_anafi.cc
@@ -12,26 +12,26 @@ using namespace std;
 int main(int argc, char **argv) {
   // Argument Checking
   if (argc < 5) {
-    cerr << endl << "Client Usage: ./test VOC_PATH SETTINGS_PATH VIDEO_PATH RUN_TYPE(client|server)" << endl;
+    cerr << endl << "Client Usage: ./test VOC_PATH SETTINGS_PATH RUN_TYPE(client|server) VIDEO_PATH" << endl;
     return -1;
   }
 
-  // Open video file
-  std::string videoPath = argv[3];
-  cv::VideoCapture videoCap(videoPath);
-
-  if (!videoCap.isOpened()) {
-    cerr << endl << "Error opening video file!" << endl;
-    return -1;
-  }
-  
   // Check runtype and convert to lowercase
-  std::string RunType(argv[4]);
+  std::string RunType(argv[3]);
   std::transform(RunType.begin(), RunType.end(), RunType.begin(), ::tolower);
 
   // Client
   if (RunType.compare("client") == 0) {
-    
+
+    // Open video file
+    std::string videoPath = argv[4];
+    cv::VideoCapture videoCap(videoPath);
+
+    if (!videoCap.isOpened()) {
+      cerr << endl << "Error opening video file!" << endl;
+      return -1;
+    }
+
     // Save frames and timestamps in a vector
     vector<cv::Mat> frames;
     vector<double> timestamps;
@@ -126,8 +126,8 @@ int main(int argc, char **argv) {
   }
 
   else {
-    cerr << endl << "Client Usage: ./mono_anafi VOC_PATH SETTINGS_PATH VIDEO_PATH RUN_TYPE(client|server)" << endl;
-    cerr << endl << "Server Usage: ./mono_anafi VOC_PATH SETTINGS_PATH VIDEO_PATH RUN_TYPE(client|server)" << endl;
+    cerr << endl << "Client Usage: ./mono_anafi VOC_PATH SETTINGS_PATH RUN_TYPE(client|server) VIDEO_PATH" << endl;
+    cerr << endl << "Server Usage: ./mono_anafi VOC_PATH SETTINGS_PATH RUN_TYPE(client|server) VIDEO_PATH" << endl;
     return -1;
   }
 
